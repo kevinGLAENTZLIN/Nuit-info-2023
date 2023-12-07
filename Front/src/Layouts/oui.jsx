f8a48ea91cb6a4b09587faaf2e9d6a225e333225
kevin glaentzlin <kevin@MacBook-Pro-de-kevin.local>
2023-12-07 23:45:38 +0100

    [+] add first preview of site

--- Front/src/Layouts/oui.jsx
+++ Front/src/Layouts/oui.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { Button, Container, Typography, Card, CardContent, Grid, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import Logo from './pein3.jpeg';
 
 const sections = [
-  { title: 'Section 1', content: 'Contenu de la section 1 pour réduire l\'impact environnemental.', image: 'url_to_section_1_image', link: 'section1', bottomContent: 'Description unique pour la section 1.' },
-  { title: 'Section 2', content: 'Contenu de la section 2 pour réduire l\'impact environnemental.', image: 'url_to_section_2_image', link: 'section2', bottomContent: 'Description unique pour la section 2.' },
-  { title: 'Section 3', content: 'Contenu de la section 3 pour réduire l\'impact environnemental.', image: 'url_to_section_3_image', link: 'section3', bottomContent: 'Description unique pour la section 3.' },
-  { title: 'Section 4', content: 'Contenu de la section 4 pour réduire l\'impact environnemental.', image: 'url_to_section_4_image', link: 'section4', bottomContent: 'Description unique pour la section 4.' },
+  {
+    title: 'L\'impact environnemental de nos actions',
+    content: 'Découvrez comment nos actions contribuent à la préservation de l\'environnement.',
+    image: Logo,
+    link: 'L\'impact environnemental de nos actions',
+    environmentalDescription: 'Explorez l\'impact positif que nos actions ont sur la planète et comment nous contribuons à la préservation de la biodiversité.',
+  },
+  {
+    title: 'Mythe vs Réalité sur l\'impact environnemental',
+    content: 'Démystifions les idées préconçues sur l\'impact environnemental de différentes pratiques.',
+    image: 'url_to_section_2_image',
+    link: 'Mythe vs Réalité sur l\'impact environnemental',
+    environmentalDescription: 'Découvrez la vérité derrière certaines idées reçues sur l\'impact environnemental. Comment nos choix quotidiens peuvent-ils influencer positivement la planète?',
+  },
+  {
+    title: 'Actions concrètes pour un impact positif',
+    content: 'Découvrez les actions pratiques que nous entreprenons pour avoir un impact positif sur l\'environnement.',
+    image: 'url_to_section_3_image',
+    link: 'Actions concrètes pour un impact positif',
+    environmentalDescription: 'Explorez les initiatives que nous prenons pour réduire notre empreinte écologique. Ensemble, nous pouvons créer un changement significatif.',
+  },
+  {
+    title: 'L\'importance de la sensibilisation',
+    content: 'Explorez comment la sensibilisation peut jouer un rôle crucial dans la protection de l\'environnement.',
+    image: 'url_to_section_4_image',
+    link: 'L\'importance de la sensibilisation',
+    environmentalDescription: 'Découvrez comment sensibiliser les autres peut être un catalyseur pour la protection de l\'environnement. Comment votre implication peut-elle inspirer des actions positives?',
+  },
 ];
 
 export default function LandingPage() {
@@ -32,7 +57,6 @@ export default function LandingPage() {
         {sections.map((section, index) => (
           <Grid item key={index} xs={3}>
             <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-              {/* Add your image here */}
               <img src={section.image} alt={`Section ${index + 1}`} style={{ width: '100%', height: '150px', objectFit: 'cover' }} />
               <CardContent sx={{ flexGrow: 1 }}>
                 <Typography variant="h5" component="div">
@@ -57,6 +81,11 @@ export default function LandingPage() {
           <Typography>
             {section.bottomContent}
           </Typography>
+          <Box sx={{ marginTop: 2 }}>
+            <Typography>
+              {section.environmentalDescription}
+            </Typography>
+          </Box>
         </Box>
       ))}
     </Container>
