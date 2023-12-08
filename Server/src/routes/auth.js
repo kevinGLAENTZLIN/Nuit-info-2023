9e95539ceab589d10770e5023f5a6cebc80a30a8
Azurioh <80247703+Azurioh@users.noreply.github.com>
2023-12-08 06:12:05 +0100

    [+] Edit msg response

--- Server/src/routes/auth.js
+++ Server/src/routes/auth.js
@@ -32,22 +32,22 @@ router.post("/activate", (req, res) => {
     try {
         db.getUserByEmail(email).then((rows) => {
             if (rows[0] === undefined) {
-                res.status(400).json({ msg: "Invalid Credentials" });
+                res.status(400).json({ msg: "Compte inexistant. Veuillez réessayer" });
                 return;
             }
             if (rows[0].activate_code === 1) {
-                res.status(400).json({ msg: "Account already activated" });
+                res.status(400).json({ msg: "Votre compte a déjà été activé" });
                 return;
             }
             db.updateUser(rows[0].id, 'activate_code = true').then((rows) => {
-                res.status(201).json({ msg: "Account activated" });
+                res.status(201).json({ msg: "Compte créé. Veuillez cliquer sur le lien reçu par mail" });
             }).catch((err) => {
-                res.status(500).json({ msg: "Internal server error", error: err });
+                res.status(500).json({ msg: "Erreur de serveur, veuillez réessayer ultérieurement", error: err });
                 console.error(err);
             });
         });
     } catch (err) {
-        res.status(403).json({ msg: "Internal server error" });
+        res.status(403).json({ msg: "Erreur de serveur, veuillez réessayer ultérieurement" });
     }
 });
 
@@ -57,32 +57,32 @@ router.post("/login", async (req, res) => {
     const captchaValue = req.body.captcha;
 
     if (!captchaValue || !email || !password) {
-        res.status(400).json({ msg: "Bad parameter" });
+        res.status(400).json({ msg: "Données invalides, veuillez réessayer" });
         return;
     }
 
     if (await verifyCaptcha(captchaValue) === false && captchaValue !== process.env.SECRET) {
-        res.status(400).json({ msg: "Invalid captcha" });
+        res.status(400).json({ msg: "Captcha invalide, veuillez réessayer" });
         return;
     }
 
     db.getUserByEmail(email, true).then((rows) => {
         if (rows[0] === undefined) {
-            res.status(400).json({ msg: "Invalid Credentials" });
+            res.status(400).json({ msg: "Email et/ou mot de passe invalide" });
             return;
         }
         if (rows[0].activate_code === 0) {
-            res.status(400).json({ msg: "Account not activated" });
+            res.status(400).json({ msg: "Votre compte n'a pas encore été activé" });
             return;
         }
         if (bcrypt.compareSync(password, rows[0].password)) {
             let token = jwt.sign({ id: `${rows[0].id}` }, process.env.SECRET, { expiresIn: '4w' });
             res.status(201).json({ token: token, id: rows[0].id });
         } else {
-            res.status(400).json({ msg: "Invalid Credentials" });
+            res.status(400).json({ msg: "Email et/ou mot de passe invalide" });
         }
     }).catch((err) => {
-        res.status(500).json({ msg: "Internal server error", error: err });
+        res.status(500).json({ msg: "Erreur de serveur, veuillez réessayer ultérieurement", error: err });
         console.error(err);
     });
 });
@@ -91,7 +91,7 @@ router.post("/register", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     if (!email || !password || !checkEmail(email) || !checkPassword(password)) {
-        res.status(400).json({ msg: "Bad parameter" });
+        res.status(400).json({ msg: "Données invalides, veuillez réessayer" });
         return;
     }
 
@@ -99,7 +99,7 @@ router.post("/register", (req, res) => {
 
     db.getUserByEmail(email).then((rows) => {
         if (rows[0] !== undefined) {
-            res.status(400).json({ msg: "Email already exists" });
+            res.status(400).json({ msg: "Cette email a déjà été utilisé. Veuillez en choisir une nouvelle" });
             return;
         }
 
@@ -109,21 +109,21 @@ router.post("/register", (req, res) => {
                 const signedCode = crypto.createHmac('sha256', process.env.SECRET).update(randomCode).digest('hex');
                 const link = `${process.env.API_URL}/activate?code=${encodeURIComponent(randomCode)}&signature=${encodeURIComponent(signedCode)}&email=${encodeURIComponent(email)}`;
                 if (process.env.NODE_ENV === 'dev') {
-                    res.status(201).json({ msg: "Account created", link: link });
+                    res.status(201).json({ msg: "Compte créé. Veuillez cliquer sur le lien reçu par mail", link: link });
                     return;
                 }
                 emailSender.sendEmail(email, link, extractUsername(email));
-                res.status(201).json({ msg: "Account created" });
+                res.status(201).json({ msg: "Compte créé. Veuillez cliquer sur le lien reçu par mail" });
             } catch (err) {
-                res.status(500).json({ msg: "Error creating account", error: err });
+                res.status(500).json({ msg: "Impossible de créer le compte, veuillez réessayer", error: err });
                 console.error(err);
             }
         }).catch((err) => {
-            res.status(500).json({ msg: "Error creating account", error: err });
+            res.status(500).json({ msg: "Impossible de créer le compte, veuillez réessayer", error: err });
             console.error(err);
         });
     }).catch((err) => {
-        res.status(500).json({ msg: "Error connexion database", error: err });
+        res.status(500).json({ msg: "Impossible de créer le compte, veuillez réessayer", error: err });
         console.error(err);
     });
 });
